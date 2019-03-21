19fc3ee45af6461781939994cd7ce5b76b67e9da
euxcet <zcc.qwer@gmail.com>
2019-03-21 08:01:04 -0700

    challenge 1

--- labcodes/lab1/kern/trap/trap.c
+++ labcodes/lab1/kern/trap/trap.c
@@ -141,6 +141,8 @@ print_regs(struct pushregs *regs) {
     cprintf("  eax  0x%08x\n", regs->reg_eax);
 }
 
+struct trapframe temp_k2u, *temp_u2k;
+
 /* trap_dispatch - dispatch based on what type of trap occurred */
 static void
 trap_dispatch(struct trapframe *tf) {
@@ -169,20 +171,49 @@ trap_dispatch(struct trapframe *tf) {
         break;
     //LAB1 CHALLENGE 1 : YOUR CODE you should modify below codes.
     case T_SWITCH_TOU:
-    case T_SWITCH_TOK:
-        panic("T_SWITCH_** ??\n");
-        break;
-    case IRQ_OFFSET + IRQ_IDE1:
-    case IRQ_OFFSET + IRQ_IDE2:
-        /* do nothing */
-        break;
-    default:
-        // in kernel, it must be a mistake
-        if ((tf->tf_cs & 3) == 0) {
-            print_trapframe(tf);
-            panic("unexpected trap in kernel.\n");
-        }
-    }
+		if (tf -> tf_cs != USER_CS) {
+			tf -> tf_cs = USER_CS;
+			tf -> tf_ds = tf -> tf_es = tf -> tf_ss = USER_DS;
+			tf -> tf_eflags |= FL_IOPL_MASK;
+			/*
+			temp_k2u = *tf;
+			temp_k2u = *tf;
+			temp_k2u.tf_cs = USER_CS;
+			temp_k2u.tf_ds = temp_k2u.tf_es = temp_k2u.tf_ss = USER_DS;
+			temp_k2u.tf_esp = (uint32_t)tf + sizeof(struct trapframe) - 8;
+			temp_k2u.tf_eflags |= FL_IOPL_MASK;
+			*((uint32_t *)tf - 1) = (uint32_t)&temp_k2u;
+			*/
+		}
+		break;
+
+	case T_SWITCH_TOK:
+		if (tf->tf_cs != KERNEL_CS) {
+			tf -> tf_cs = KERNEL_CS;
+			tf -> tf_ds = tf -> tf_es = tf -> tf_ss = KERNEL_DS;
+			tf -> tf_eflags &= ~FL_IOPL_MASK;
+			/*
+			tf->tf_cs = KERNEL_CS;
+			tf->tf_ds = tf->tf_es = KERNEL_DS;
+			tf->tf_eflags &= ~FL_IOPL_MASK;
+			temp_u2k = (struct trapframe *)(tf->tf_esp - (sizeof(struct trapframe) - 8));
+			memmove(temp_u2k, tf, sizeof(struct trapframe) - 8);
+			*((uint32_t *)tf - 1) = (uint32_t)temp_u2k;
+			*/
+		}
+		break;
+
+	case IRQ_OFFSET + IRQ_IDE1:
+	case IRQ_OFFSET + IRQ_IDE2:
+		/* do nothing */
+		break;
+	default:
+		// in kernel, it must be a mistake
+		if ((tf->tf_cs & 3) == 0) {
+			print_trapframe(tf);
+			panic("unexpected trap in kernel.\n");
+		}
+	}
 }
 
 /* *
@@ -192,7 +223,7 @@ trap_dispatch(struct trapframe *tf) {
  * */
 void
 trap(struct trapframe *tf) {
-    // dispatch based on what type of trap occurred
-    trap_dispatch(tf);
+	// dispatch based on what type of trap occurred
+	trap_dispatch(tf);
 }
 
